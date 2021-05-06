0e33d9d37774c5a55d87e202baf520baa22f8bd7
aioneersSZ <74610865+aioneersSZ@users.noreply.github.com>
2021-05-06 16:59:55 +0200

    Revert "Removed aioconnect from aiox"

--- aiox/__init__.py
+++ aiox/__init__.py
@@ -4,23 +4,12 @@ from .xyz_analysis import xyz_analysis
 
 from .create_time_series import create_time_series
 
+from .azure_key_vault import vault_get_secret
+from .azure_key_vault import _vault_set_dbutils
+from .azure_key_vault import _is_running_on_databricks
+from .azure_key_vault import _is_running_on_devops_pipeline
+
 from .read_and_write import read_and_write, read_and_write_all, read_and_concat, read_all_sheets
-
-
-def _vault_set_dbutils(dbutils_var: str):
-    """Allows the vault functions to use the ``dbutils`` variable
-
-    Parameters
-    ----------
-        dbutils_var
-            ``dbutils`` variable from Databricks should be passed here
-
-    This function sets the variable ``dbutils`` to be used when running Databricks scripts.
-    """
-
-    global dbutils
-    dbutils = dbutils_var
-
 
 def set_dbutils(dbutils_var):
     """Allows the vault functions to use the ``dbutils`` variable
